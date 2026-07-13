bb38dc9fe055869aaeff4d6bd43b4b7ceb84f1c6
Radhe <theradhe.s@gmail.com>
2026-07-13 17:54:16 +0530

    updated init for new features(commit)

--- src/init.cpp
+++ src/init.cpp
@@ -1,13 +1,14 @@
-#include "../include/repository.h"
+#include "../include/init.h"
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <ostream>
 
 namespace fs = std::filesystem;
 
 void init() {
 
-    if (fs::exists(".mygit")) {
+    if (fs::exists(".minigit")) {
         std::cout << "Repository already initialized\n";
         return;
     }
@@ -16,9 +17,20 @@ void init() {
     fs::create_directory(".minigit/objects");
     fs::create_directory(".minigit/refs");
 
-    std::ofstream head(".mygit/HEAD");
+    std::ofstream head(".minigit/HEAD");
     head << "refs/main";
     head.close();
+
+
+    std::ofstream index(".minigit/index");
+    index.close();
+
+    std::ofstream mainRef(".minigit/refs/main");
+    mainRef.close();
+
+    std::ofstream nextObject(".minigit/next_object");
+    nextObject << 1;
+    nextObject.close();
 
     std::cout << "Initialized empty MiniGit repository\n";
 }
